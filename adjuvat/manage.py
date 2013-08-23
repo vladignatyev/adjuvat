403f56d913e6687b43d2d54d55c5d5f145305e49
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-08-23 11:16:26 +0200

    initial

---
+++ adjuvat/manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    if 'DEVELOPER' in os.environ:
+      os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adjuvat.settings.dev")
+    else:
+      os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adjuvat.settings.prod")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
