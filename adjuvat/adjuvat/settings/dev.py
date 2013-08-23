27f20fa01baa83c8cac8304662da9ed47bd5670c
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-08-23 11:19:17 +0200

    tweaking settings, added wsgi.py

--- adjuvat/adjuvat/settings/dev.py
+++ adjuvat/adjuvat/settings/dev.py
@@ -1,5 +1,7 @@
 # Django settings for adjuvat project.
 
+from adjuvat.settings.prod import *
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -113,18 +115,6 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
