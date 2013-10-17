a5616aa4464de4ad239ae6f9de26176c46f7037f
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-10-17 22:55:11 +0400

    added models

--- adjuvat/crm/admin.py
+++ adjuvat/crm/admin.py
@@ -1,2 +1,11 @@
+from crm.models import Order, PricingItem, Executor, Service, Client, Company
+from django.contrib import admin
+
 __author__ = 'ignatev'
 
+admin.site.register(Company)
+admin.site.register(Client)
+admin.site.register(Service)
+admin.site.register(Executor)
+admin.site.register(PricingItem)
+admin.site.register(Order)
--- adjuvat/crm/models.py
+++ adjuvat/crm/models.py
@@ -8,14 +8,16 @@ class Company(models.Model):
 
     title = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
-    info = models.TextField()
-    address = models.TextField()
+    info = models.TextField(blank=True)
+    address = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return u"%s - %s - %s" % (self.title, self.address, self.phone)
 
 
 class Client(models.Model):
     """Модель клиента компании company пользователя"""
-    SEX_CHOICES = (('NA', 'N/A'),
-                   ('M', 'Male'),
+    SEX_CHOICES = (('M', 'Male'),
                    ('F', 'Female'))
 
     hidden = models.BooleanField()
@@ -25,9 +27,12 @@ class Client(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     sex = models.CharField(choices=SEX_CHOICES, max_length=16)
-    birth_date = models.DateField(blank=True)
+    birth_date = models.DateField(blank=True, null=True)
     phone = models.CharField(max_length=255, blank=True)
     mail = models.EmailField(blank=True)
+
+    def __unicode__(self):
+        return u"%s: %s %s" % (self.company.title, self.first_name, self.last_name)
 
 
 class Service(models.Model):
@@ -36,6 +41,9 @@ class Service(models.Model):
 
     company = models.ForeignKey('Company')
     name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.company.title, self.name)
 
 
 class Executor(models.Model):
@@ -49,12 +57,22 @@ class Executor(models.Model):
 
     services = models.ManyToManyField('Service')
 
+    def __unicode__(self):
+        return u"%s: %s %s" % (self.company.title, self.first_name, self.last_name)
+
 
 class PricingItem(models.Model):
     """Модель цены услуги service, производимой исполнителем executor"""
     executor = models.ForeignKey('Executor')
     service = models.ForeignKey('Service')
     price = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return u"%s: %s %s `%s` за %s Р." % (self.executor.company.title,
+                               self.executor.first_name,
+                               self.executor.last_name,
+                               self.service.name,
+                               self.price)
 
 
 class Order(models.Model):
@@ -68,8 +86,9 @@ class Order(models.Model):
     client = models.ForeignKey('Client')
     executor = models.ForeignKey('Executor')
     services = models.ManyToManyField('Service')
-    calculated_price = models.PositiveIntegerField()
-    user_price = models.PositiveIntegerField()
+
+    calculated_price = models.PositiveIntegerField(blank=True)
+    user_price = models.PositiveIntegerField(blank=True)
 
     planned_time_start = models.DateTimeField()
     planned_time_end = models.DateTimeField()
