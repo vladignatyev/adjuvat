8760a08c26dd79d0496270f8143be3243ba9360a
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-10-30 18:33:34 +0400

    little improvements in datepicker

--- adjuvat/crm/clients/forms.py
+++ adjuvat/crm/clients/forms.py
@@ -6,7 +6,7 @@ class CreateClientForm(forms.Form):
     first_name = forms.CharField(max_length=255, required=True)
     last_name = forms.CharField(max_length=255, required=True)
     sex = forms.ChoiceField(choices=Client.SEX_CHOICES, required=True)
-    birth_date = forms.DateField()
+    birth_date = forms.DateField(required=False, input_formats=['%d.%m.%Y',])
     phone = forms.CharField(max_length=255)
     mail = forms.EmailField(required=False)
 
@@ -15,10 +15,10 @@ class CreateClientForm(forms.Form):
             return None
 
         c = Client()
-        c.first_name = self.first_name
-        c.last_name = self.last_name
-        c.sex = self.sex
-        c.birth_date = self.birth_date
-        c.phone = self.phone
-        c.mail = self.mail
+        c.first_name = self.cleaned_data['first_name']
+        c.last_name = self.cleaned_data['last_name']
+        c.sex = self.cleaned_data['sex']
+        c.birth_date = self.cleaned_data['birth_date']
+        c.phone = self.cleaned_data['phone']
+        c.mail = self.cleaned_data['mail']
         return c
