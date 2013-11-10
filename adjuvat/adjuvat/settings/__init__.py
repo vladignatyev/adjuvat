dfc7e1647c9994ca0c151eaef93cfbd2c5026421
Vladimir Ignatev <ya.na.pochte@gmail.com>
2013-11-10 21:27:37 +0400

    pep8

--- adjuvat/adjuvat/settings/__init__.py
+++ adjuvat/adjuvat/settings/__init__.py
@@ -1,4 +1,5 @@
 from django.template import add_to_builtins
+
 add_to_builtins('djangojs.templatetags.js')
 add_to_builtins('eztables.templatetags.eztables')
 add_to_builtins('django.contrib.staticfiles.templatetags.staticfiles')
